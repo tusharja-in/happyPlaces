f3c8b6a6bc8b991b02272fd0286b394b3a47049b
Tushar Jain <tusharjain4mar2002@gmail.com>
2021-08-27 01:16:39 +0530

    gallery picking feature added.

--- app/src/main/java/com/visit/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/visit/happyplaces/AddHappyPlaceActivity.kt
@@ -1,11 +1,167 @@
 package com.visit.happyplaces
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.Settings
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.appcompat.widget.AppCompatImageButton
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.Toolbar
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import java.text.SimpleDateFormat
+import java.util.*
 
-class AddHappyPlaceActivity : AppCompatActivity() {
+class AddHappyPlaceActivity : AppCompatActivity(),View.OnClickListener {
+
+    companion object {
+        private const val GALLERY=1
+    }
+
+    var cal=Calendar.getInstance()
+    private lateinit var dateSetListener:DatePickerDialog.OnDateSetListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
+        val toolbar_add_place=findViewById<Toolbar>(R.id.toolbar_add_place)
+        val etDateAdd=findViewById<AppCompatEditText>(R.id.etDateAdd)
+        val tv_add_image=findViewById<TextView>(R.id.tv_add_image)
+        val btn_save=findViewById<Button>(R.id.btn_save)
+
+        setSupportActionBar(toolbar_add_place)
+        val actionBar=supportActionBar
+
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+        actionBar?.title="Add Happy Places"
+        toolbar_add_place.setNavigationOnClickListener{
+            onBackPressed()
+        }
+
+        dateSetListener=DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR,year)
+            cal.set(Calendar.MONTH,month)
+            cal.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+            updateDateInView()
+        }
+        etDateAdd.setOnClickListener(this)
+        tv_add_image.setOnClickListener(this)
+        btn_save.setOnClickListener(this)
+
     }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.etDateAdd->{
+                val datePickerDialog=DatePickerDialog(this,dateSetListener,cal.get(Calendar.YEAR),cal.get(Calendar.MONTH),cal.get(Calendar.DAY_OF_MONTH))
+                datePickerDialog.show()
+                datePickerDialog.getButton(DatePickerDialog.BUTTON_NEGATIVE).setTextColor(Color.parseColor("#4b86b4"))
+                datePickerDialog.getButton(DatePickerDialog.BUTTON_POSITIVE).setTextColor(Color.parseColor("#4b86b4"))
+            }
+            R.id.tv_add_image->{
+                val pictureDialog=AlertDialog.Builder(this)
+                pictureDialog.setTitle("Select Action")
+                val pictureDialogItems= arrayOf("Select photo from gallery","Capture photo from camera")
+                pictureDialog.setItems(pictureDialogItems){
+                    dialog,which->
+                    when(which){
+                        0->{
+                            choosePhotoFromGallery()
+                        }
+                        1->{
+                            Toast.makeText(this,"Soon",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+                pictureDialog.show()
+            }
+
+            R.id.btn_save->{
+
+            }
+        }
+    }
+
+    private fun choosePhotoFromGallery(){
+        Dexter.withContext(this).withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE).withListener(
+            object: MultiplePermissionsListener {
+
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    if (report!!.areAllPermissionsGranted()){
+                        val galleryIntent=Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                        startActivityForResult(galleryIntent, GALLERY)
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(permissions: MutableList<PermissionRequest>?, token: PermissionToken?) {
+                    showRationalDialogForPermissions()
+                    token?.continuePermissionRequest()
+                }
+
+            }
+        ).onSameThread().check()
+
+    }
+
+    private fun showRationalDialogForPermissions(){
+        AlertDialog.Builder(this).setMessage(""+"It looks like you have turned off permissions required for this feature. However you can enable this in the settings.").
+        setPositiveButton("GO TO SETTINGS"){
+            _,_->
+            try {
+                val intent =Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri= Uri.fromParts("package",packageName,null)
+                intent.data=uri
+                startActivity(intent)
+            }
+            catch (e:Exception){
+                e.printStackTrace()
+            }
+        }.setNegativeButton("CANCEL"){
+            dialog,_->
+            dialog.dismiss()
+        }.show()
+    }
+
+
+    private fun updateDateInView(){
+        val etDateAdd=findViewById<AppCompatEditText>(R.id.etDateAdd)
+        val myFormat="dd.MM.yyyy"
+        val sdf=SimpleDateFormat(myFormat, Locale.getDefault())
+        etDateAdd.setText(sdf.format(cal.time).toString())
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode== Activity.RESULT_OK){
+            if (requestCode== GALLERY){
+                val iv_place_image=findViewById<AppCompatImageView>(R.id.iv_place_image)
+                if (data!=null){
+                    val contentURI=data.data
+                    try{
+                        val selectedImageBitmap=MediaStore.Images.Media.getBitmap(this.contentResolver,contentURI)
+                        iv_place_image.setImageBitmap(selectedImageBitmap)
+                    }catch (e:Exception){
+                        e.printStackTrace()
+                        Toast.makeText(this,"Failed!",Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
 }

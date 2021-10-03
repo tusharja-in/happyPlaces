6cac206d91282edae5f86e3eed6f9f1fe8d26a1b
Tushar Jain <tusharjain4mar2002@gmail.com>
2021-10-04 02:30:26 +0530

    recycler view created and adapter was added.

--- app/src/main/java/com/visit/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/com/visit/happyplaces/activities/MainActivity.kt
@@ -4,10 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.visit.happyplaces.R
+import com.visit.happyplaces.adapters.HappyPlacesAdapter
 import com.visit.happyplaces.database.DatabaseHandler
 import com.visit.happyplaces.models.HappyPlaceModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,11 +27,25 @@ class MainActivity : AppCompatActivity() {
         getHappyPlaceListFromLocalDB()
     }
 
+    private fun setupHappyPlacesRecyclerView(happyPlaceList:ArrayList<HappyPlaceModel>){
+        rv_happy_places_list.layoutManager=LinearLayoutManager(this)
+        rv_happy_places_list.setHasFixedSize(true)
+        val placesAdapter = HappyPlacesAdapter(this,happyPlaceList)
+        rv_happy_places_list.adapter=placesAdapter
+    }
+
     private fun getHappyPlaceListFromLocalDB(){
         val dbHandler=DatabaseHandler(this)
-        val getHappyPlaceList:ArrayList<HappyPlaceModel> = dbHandler.getHappyPlacesList()
-
-        //Log.e("size",getHappyPlaceList.size.toString())
+        val getHappyPlaceList = dbHandler.getHappyPlacesList()
+        if(getHappyPlaceList.size>0){
+            rv_happy_places_list.visibility= View.VISIBLE
+            tv_no_record_available.visibility=View.GONE
+            setupHappyPlacesRecyclerView(getHappyPlaceList)
+        }
+        else{
+            rv_happy_places_list.visibility= View.GONE
+            tv_no_record_available.visibility=View.VISIBLE
+        }
     }
 
 }
